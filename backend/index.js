b5690b52735306072a0eb4b4f7e539a4e0746fee
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2025-06-21 13:55:53 +0530

    Update index.js Fix merge conflict

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,6 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-<<<<<<< HEAD
   origin: function (origin, callback) {
     if (!origin) return callback(null, true); // Allow requests like Postman or curl with no origin
     if (allowedOrigins.indexOf(origin) !== -1) {
@@ -44,10 +43,8 @@ const corsOptions = {
     }
   },
   credentials: true,
-=======
     origin: process.env.URL,
     credentials: true
->>>>>>> ede4d5c (url changes)
 };
 
 app.use(cors(corsOptions));
